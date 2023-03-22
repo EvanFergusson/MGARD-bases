27a0175926c0e61e983b48a46a8e1bff8b06bf07
Reshniak, Viktor <reshniakv@ornl.gov>
2023-03-22 15:40:55 -0400

    Update ex_interpolate.py

--- ex_interpolate.py
+++ ex_interpolate.py
@@ -12,7 +12,8 @@ from mgard import MGARD
 
 N = 6 * 2**6 + 1
 
-grid = np.linspace(0,1,N)
+# grid = np.linspace(0,1,N)
+grid = np.logspace(0,1,N)
 ind0 = np.arange(0,N,2)
 dind = np.arange(1,N,2)
 
@@ -36,6 +37,7 @@ mg0 = MGARD(grid, u, order=0)
 mg1 = MGARD(grid, u, order=1)
 mg2 = MGARD(grid, u, order=2)
 mg3 = MGARD(grid, u, order=3)
+mg4 = MGARD(grid, u, order=4)
 
 
 u0 = u.copy()
@@ -50,6 +52,9 @@ u2[dind] = mg2.interpolate(ind0, dind, u[ind0])
 u3 = u.copy()
 u3[dind] = mg3.interpolate(ind0, dind, u[ind0])
 
+u4 = u.copy()
+u4[dind] = mg4.interpolate(ind0, dind, u[ind0])
+
 
 plt.subplot(121)
 plt.plot(grid, u,  label='u')
@@ -57,6 +62,7 @@ plt.step(grid, u0, label='u0')
 plt.plot(grid, u1, label='u1')
 plt.plot(grid, u2, label='u2')
 plt.plot(grid, u3, label='u3')
+plt.plot(grid, u4, label='u4')
 plt.gca().set_box_aspect(1)
 plt.gca().title.set_text('Interpolants')
 plt.legend()
@@ -67,6 +73,7 @@ plt.semilogy(grid[dind], np.abs(u0-u)[1::2])
 plt.semilogy(grid[dind], np.abs(u1-u)[1::2])
 plt.semilogy(grid[dind], np.abs(u2-u)[1::2])
 plt.semilogy(grid[dind], np.abs(u3-u)[1::2])
+plt.semilogy(grid[dind], np.abs(u4-u)[1::2])
 plt.gca().set_box_aspect(1)
 plt.gca().title.set_text('Errors')
 
